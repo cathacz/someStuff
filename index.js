9ffd363abe2ea3d698536fca2ee343c856f2e665
catha <catharinaczipf@hotmail.com>
2021-05-04 15:41:14 +0200

    I am overthinking this

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ const result = document.querySelector(".result");
 const userInput = document.querySelector("#userInput").value;
 const system = document.querySelector("#system");
 function registerUserValue(e) {
-  console.dir(e.target);
   userNumber = e.target.value;
 }
 
 function convert() {
   let currentSystem;
+  var f = document.getElementById("sys");
+  var sys = f.options[f.selectedIndex].value;
   console.log(system.value);
   switch (system.value) {
-    case "binary":
-      currentSystem = parseInt(userInput, 2);
+    case "bin":
+      currentSystem = parseInt(userNumber, 2);
       break;
-    case "decimal":
-      currentSystem = parseInt(userInput, 10);
+    case "dec":
+      currentSystem = parseInt(userNumber, 10);
       break;
-    case "hexadecimal":
-      currentSystem = parseInt(userInput, 16);
+    case "hex":
+      currentSystem = parseInt(userNumber, 16);
       break;
     default:
-      currentSystem = parseInt(userInput, 10);
+      currentSystem = parseInt(userNumber, 10);
   }
-  var f = document.getElementById("sys");
-  var sys = f.options[e.selectedIndex].value;
 
   result.innerHTML = `${parseInt(currentSystem, sys)}`;
 }
 
 system.addEventListener("change", convert);
-// userInput.addEventListener("keyup", registerUserValue);
+userInput.addEventListener("keyup", registerUserValue);
