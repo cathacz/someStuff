53f56dbc04689dfa0b1bd19527388c464a01d3aa
catha <catharinaczipf@hotmail.com>
2021-05-06 15:35:54 +0200

    next crap

--- index.js
+++ index.js
@@ -47,3 +47,17 @@ function convert(e) {
 }
 sys.addEventListener("change", convert);
 userInput.addEventListener("keyup", registerUserValue);
+
+let slider = document.querySelector("#slider").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $(".foreground-img").css(800, `${sliderPos}%`);
+});
+
+$("#slider").on("input change", (e) => {
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $(".foreground-img").css("width", `${sliderPos}%`);
+  // Update the position of the slider button
+  $(".slider-button").css("left", `calc(${sliderPos}% - 18px)`);
+});
